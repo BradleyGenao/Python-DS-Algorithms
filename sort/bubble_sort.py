99e191a3045780812abddcbf2f2e198475453711
Bradley Genao <bjg1889@gmail.com>
2022-02-17 16:19:52 -0800

    Added Bubble sort class

---
+++ sort/bubble_sort.py
@@ -0,0 +1,24 @@
+class BubbleSort:
+    def __init__(self, arr = [3,4,2,1,5] ):
+        self.arr = arr    
+        self.size = len(arr)
+       
+    def print_array(self):
+        for index, num in enumerate(self.arr):
+            print("Index: {} Element: {}".format(index, num))
+    
+    def sort(self):
+        for _ in range(self.size):
+            for i in range(self.size - 1):
+                if self.arr[i] > self.arr[i+1]:
+                    self.arr[i], self.arr[i+1] = self.arr[i+1], self.arr[i]
+        return self.arr 
+            
+
+
+test = BubbleSort([4,3,1,2])
+sorted_array = BubbleSort(test.arr)
+test.print_array()
+print("***********Sorted****************")
+sorted_array.sort()
+sorted_array.print_array()
